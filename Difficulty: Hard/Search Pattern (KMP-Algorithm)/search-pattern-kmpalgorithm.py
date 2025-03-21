b23a95f4fe2ed750a4a859c17de878d17c59adc2
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2025-03-21 17:41:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def search(self, pat, txt):
+        result = []
+
+        m, n = len(pat), len(txt)
+
+        for i in range(n - m + 1):
+            if txt[i:i + m] == pat:
+                result.append(i)
+
+        return result
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        patt = input().strip()
+        ob = Solution()
+        ans = ob.search(patt, s)
+        if len(ans) == 0:
+            print("[]", end="")
+        for value in ans:
+            print(value, end=' ')
+        print()
+
+# } Driver Code Ends
