9e62438fc1f675d270f1fd8ddcf622db5d450540
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2025-03-23 16:38:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -0,0 +1,22 @@
+class Solution:
+    def knapsack(self, W, val, wt):
+        dp = [0] * (W + 1)
+
+        for i in range(1, len(wt) + 1):
+            for j in range(W, wt[i - 1] - 1, -1):
+                dp[j] = max(dp[j], dp[j - wt[i - 1]] + val[i - 1])
+        
+        return dp[W]
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    test_cases = int(input())
+    for _ in range(test_cases):
+        capacity = int(input())
+        values = list(map(int, input().strip().split()))
+        weights = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.knapsack(capacity, values, weights))
+        print("~")
+# } Driver Code Ends
