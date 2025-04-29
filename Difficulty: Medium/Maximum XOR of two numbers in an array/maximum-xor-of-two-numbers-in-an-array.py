dc32288030f76178e0a89a9620381c6f1d1c3498
Mehul Khandelwal <168804011+Mehul-kh2005@users.noreply.github.com>
2025-04-29 09:10:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum XOR of two numbers in an array/maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    def maxXor(self, arr):
+        res = 0
+        mask = 0
+    
+        s = set()
+    
+        for i in range(30, -1, -1):
+            mask |= (1 << i)
+    
+            for num in arr:
+                s.add(num & mask)
+    
+            cur = res | (1 << i)
+    
+            for prefix in s:
+                if cur ^ prefix in s:
+                    res = cur
+                    break
+    
+            s.clear()
+    
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        print(ob.maxXor(arr))
+        print("~")
+
+# } Driver Code Ends
